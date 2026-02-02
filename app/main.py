50f73e4426de9922e1d6aee8ac51fbb7bb749deb
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-02-02 20:35:16 +0100

    feat(db) : Configuration de l'environnement Alembic avec les modèles et l'URL dynamique

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.db.session import engine, Base
+from app.db.models.user import User
+from app.db.models.query import Query
 
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
