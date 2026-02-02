a21e7cbbffaa9b669e8f54dcdd4533619a973e51
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-02-02 17:20:41 +0100

    feat(config) : Mise en place de la configuration globale avec pydantic-settings

---
+++ app/core/config.py
@@ -0,0 +1,35 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings) :
+    PROJECT_NAME : str
+    VERSION : str
+    API_V1_STR : str
+    
+    POSTGRES_SERVER : str
+    POSTGRES_USER : str
+    POSTGRES_PASSWORD : str
+    POSTGRES_DB : str
+    POSTGRES_PORT : int
+    
+    DATABASE_URL : str
+    
+    JWT_SECRET : str
+    ALGORITHM : str
+    ACCESS_TOKEN_EXPIRE_MINUTES : int
+    
+    OLLAMA_BASE_URL : str
+    LLM_MODEL : str
+    EMBEDDING_MODEL : str
+    
+    CHROMA_PERSIST_DIRECTORY : str
+    UPLOADS_DIRECTORY : str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True,
+        extra="ignore"
+    )
+    
+
+settings = Settings()
