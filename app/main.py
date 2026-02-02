4c09d1d8fac3a9596dd10409ae6f493372739f17
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-02-02 17:26:24 +0100

    feat(api) : Initialisation du point d'entrée FastAPI dans app/main.py

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION
+)
+
+@app.get("/")
+async def root() :
+    return {
+        "status": "online",
+        "project": settings.PROJECT_NAME,
+        "version": settings.VERSION
+    }
