fca5cb488a2d9e821ed63a6a890831cbf153d3ff
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-02-02 21:53:39 +0100

    feat(api) : Finalisation du point d'entrée avec création des tables et enregistrement des routes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from app.core.config import settings
 from app.db.session import engine, Base
 from app.db.models.user import User
 from app.db.models.query import Query
+from app.api.v1.api import api_router
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,6 +12,10 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION
 )
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
 
 @app.get("/")
 async def root() :
