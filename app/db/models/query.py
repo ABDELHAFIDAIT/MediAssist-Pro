37b8b1aeb272b185af5740cd885d9b1cf31defac
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-02-10 11:36:23 +0100

    fix(models) : Correction du modèle Query en supprimant la contrainte Unique en colonne Response

--- app/db/models/query.py
+++ app/db/models/query.py
@@ -10,7 +10,7 @@ class Query(Base) :
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
     query = Column(Text, nullable=False)
-    response = Column(Text, nullable=False, unique=True)
+    response = Column(Text, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     
     user = relationship("User", back_populates="queries")
